f887e5fcea1fccbf66ecba5c50865ffe04d13052
Jeroen van den Enden <info@endroid.nl>
2021-10-31 00:29:39 +0200

    Remove finder

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,14 +2,8 @@
 
 declare(strict_types=1);
 
-$finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__.'/src')
-    ->in(__DIR__.'/tests')
-;
-
 $config = new PhpCsFixer\Config();
 $config
-    ->setFinder($finder)
     ->setRiskyAllowed(true)
     ->setUsingCache(false)
     ->setRules([
